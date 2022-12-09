f3fabe18b22e7a297f95c9fb5896ce708df8b18a
saurabh-malik <malik.saurabh@hotmail.com>
2022-12-08 23:28:40 -0800

    Finetuned BERT model with checkpoint

--- train_bert.py
+++ train_bert.py
@@ -5,23 +5,39 @@ import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
+import matplotlib.pyplot as plt
+
 from official.nlp import optimization  # to create AdamW optimizer
 from string import Template
 from sklearn.model_selection import train_test_split
+from tensorflow import keras
 
 from model.data_processing import load_data
 from ConvertToLanguage import convert_to_unstructure
+from ConvertToLanguage import convert_to_unstructure_for_pretraining
+from model.utils import print_my_examples
+from model.custom_loss import Custom_CE_Loss
+
 
 
 #import matplotlib.pyplot as plt
 AUTOTUNE = tf.data.AUTOTUNE
-batch_size = 64
+batch_size = 16
 seed = 42
+init_lr = 3e-5
+hp_dropout = 0.1
+epochs = 5
+
+print('Training For batch_size: ', str(batch_size))
+print('Training For lr: ', str(init_lr))
+print('Training For dropout: ', str(hp_dropout))
 #Load Data
 dataframe = load_data("data/visits", "/visitdataclassification-4300.csv")
 dataframe.head()
-train_x, hold_x = train_test_split(dataframe, test_size=0.30)
+train_x, hold_x = train_test_split(dataframe, test_size=0.20)
 
+#data for pretrain
+convert_to_unstructure_for_pretraining(dataframe)
 processedFolder = "data/visits/train"
 if(os.path.exists(processedFolder) == False):
     convert_to_unstructure(train_x)
@@ -31,7 +47,7 @@ if(os.path.exists(processedFolder) == False):
 raw_train_ds = tf.keras.utils.text_dataset_from_directory(
     'data/visits/train',
     batch_size=batch_size,
-    validation_split=0.2,
+    validation_split=0.25,
     subset='training',
     seed=seed)
 
@@ -41,7 +57,7 @@ train_ds = raw_train_ds.cache().prefetch(buffer_size=AUTOTUNE)
 val_ds = tf.keras.utils.text_dataset_from_directory(
     'data/visits/train',
     batch_size=batch_size,
-    validation_split=0.2,
+    validation_split=0.25,
     subset='validation',
     seed=seed)
 
@@ -53,18 +69,15 @@ test_ds = tf.keras.utils.text_dataset_from_directory(
 
 test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
 
 for text_batch, label_batch in train_ds.take(1):
-  for i in range(3):
+  for i in range(10):
     print(f'Review: {text_batch.numpy()[i]}')
     label = label_batch.numpy()[i]
     print(f'Label : {label} ({class_names[label]})')
 
 #Select Model
-#bert_model_name = 'small_bert/bert_en_uncased_L-4_H-512_A-8' 
-bert_model_name = 'bert_en_uncased_L-12_H-768_A-12' 
+bert_model_name = 'small_bert/bert_en_uncased_L-4_H-512_A-8'
 
 map_name_to_handle = {
     'bert_en_uncased_L-12_H-768_A-12':
@@ -214,15 +227,20 @@ bert_model = hub.KerasLayer(tfhub_handle_encoder)
 
 bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
 
-#text_test = [train_sentence[0]]
-#text_preprocessed = bert_preprocess_model(text_test)
-'''
-print(f'Keys       : {list(text_preprocessed.keys())}')
-print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
-print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
-print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
-print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
-'''
+checkpoint_path = "trained-models/pretraining_output/model.ckpt-20"
+
+def build_BERT_model():
+  text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
+  preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+  encoder_inputs = preprocessing_layer(text_input)
+  encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+  outputs = encoder(encoder_inputs)
+  net = outputs['pooled_output']
+  model = tf.keras.Model(text_input, net)
+  checkpoint = tf.train.Checkpoint(model)
+  checkpoint.restore(checkpoint_path)
+  #model.load_weights(checkpoint_path)
+  return model
 
 def build_classifier_model(classes):
   text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
@@ -231,39 +249,85 @@ def build_classifier_model(classes):
   encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
   outputs = encoder(encoder_inputs)
   net = outputs['pooled_output']
-  net = tf.keras.layers.Dropout(0.3)(net)
-  net = tf.keras.layers.Dense(classes, activation=None, name='classifier')(net)
-  return tf.keras.Model(text_input, net)
+  net = tf.keras.layers.Dropout(0.1)(net)
+  net = tf.keras.layers.Dense(classes, activation="softmax", name='classifier')(net)
+  model = tf.keras.Model(text_input, net)
+  checkpoint = tf.train.Checkpoint(model)
+  checkpoint.restore(checkpoint_path)
+  return model
 
 
+#bert_model = build_BERT_model()
+#save model
+#bert_model.save('bertmodel')
 
-classifier_model = build_classifier_model(len(class_names))
-#bert_raw_result = classifier_model(tf.constant(text_test))
+language_model = build_classifier_model(len(class_names))
+#bert_raw_result = language_model(tf.constant(text_test))
 #print(tf.sigmoid(bert_raw_result))
 
-#tf.keras.utils.plot_model(classifier_model)
+#tf.keras.utils.plot_model(language_model)
 
 loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False,ignore_class=None,
     name='sparse_categorical_crossentropy')
+#metrics = tf.keras.metrics.Recall()
 metrics = tf.keras.metrics.SparseCategoricalAccuracy('accuracy', dtype=tf.float32)
 
-epochs = 20
 steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
 num_train_steps = steps_per_epoch * epochs
 num_warmup_steps = int(0.1*num_train_steps)
 
 #init_lr = 1e-3
-init_lr = 3e-5
+init_lr = 3e-5 
 optimizer = optimization.create_optimizer(init_lr=init_lr,
                                           num_train_steps=num_train_steps,
                                           num_warmup_steps=num_warmup_steps,
                                           optimizer_type='adamw')
 
-classifier_model.compile(optimizer=optimizer,
+language_model.compile(optimizer=optimizer,
                          loss=loss,
                          metrics=metrics)
 
 print(f'Training model with {tfhub_handle_encoder}')
-history = classifier_model.fit(x=train_ds,
+
+#checkpoint
+
+posttraining_checkpoint_path = "trained-models/language/training_CustomLoss_2/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=posttraining_checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+print('Model prediction initially')
+#language_model.predict()
+examples = [
+    #'A 29 months old, 50 lb, Male Dalmatian-Canine is checked-in on Monday in hospital for Exam Annual due to Vaccines.',  # this is the same sentence tried earlier
+    'A 45 months old, 72 lb, Male Labrador Retriever Mix-Canine is checked-in on Tuesday in hospital for Technician Appointment due to Exam.'
+]
+
+#original_results = tf.sigmoid(language_model(tf.constant(examples)))
+
+
+history = language_model.fit(x=train_ds,
                                validation_data=val_ds,
-                               epochs=epochs)
+                               epochs=epochs,
+                               callbacks = [cp_callback])
+
+#language_model.load_weights(checkpoint_path)
+
+post_finetunning_results = tf.sigmoid(language_model(tf.constant(examples)))
+
+#print('Results from the model without training:')
+#print(original_results)
+#print_my_examples(examples, original_results)
+#plt.plot(original_results[0], linestyle = 'dotted')
+#plt.title('Visit Time Window before Fine Tunning distribution')
+#plt.show()
+
+print('Results from the saved model:')
+print(post_finetunning_results)
+print_my_examples(examples, post_finetunning_results)
+plt.plot(post_finetunning_results[0], linestyle = 'dotted')
+plt.title('Visit Time Window Post Fine Tunning distribution')
+plt.show()
